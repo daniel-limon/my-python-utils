62d8fb5f847814001aeb67d7318c238b0cceff47
Dan Limón <58156106+daniel-limon@users.noreply.github.com>
2020-04-07 22:58:07 -0400

    Update subnetcalcv4.py

--- subnetcalcv4.py
+++ subnetcalcv4.py
@@ -4,37 +4,19 @@
 
 import argparse
 import ipaddress
-import sys
 
 # statics
-PROGRAMDESCRIPTION = 'Returns Subnet Information For Given IP and Subnet Mask'
+PROGRAMDESCRIPTION = 'Returns IP Information For Given IP and Subnet Mask'
 HELPIPADDR = 'IPv4 address. Example: 192.168.1.100'
 HELPSUBNETMASK = 'Subnet mask. Example: 255.255.255.0'
 
 ####################################
-# this validates the ip address and subnet mask.
-# returns network address if it checks out, false if it fails
+# returns network address if it checks out, raises an error if it fails.
 def get_network_address(ipa, snm):
 
-    # this will catch error if there is an illegal octet,
-    # or an improperly formatted ipv4 address.
-    try:
-        # calculate network address
-        ntwk_addr_int = int(ipaddress.IPv4Address(ipa)) & int(ipaddress.IPv4Address(snm))
-    except:
-        return 'False'
+    ntwk_addr_int = int(ipaddress.IPv4Address(ipa)) & int(ipaddress.IPv4Address(snm))
 
-    # converts network address from type int to type str
-    ntwk_addr = str(ipaddress.ip_address(ntwk_addr_int)) + '/' + snm
-
-    # this will catch error if the subnet mask is invalid
-    try:
-        ipaddress.IPv4Network(ntwk_addr)
-    except:
-        return 'False'
-    
-    # return address string if values are good
-    return ntwk_addr
+    return str(ipaddress.ip_address(ntwk_addr_int)) + '/' + snm
 ####################################
 # command-line parser
 def get_arguments():
@@ -49,18 +31,13 @@ def start_subnet_calc():
     # get command line arguments
     clArgs = get_arguments()
 
-    # validates good ip address and subnet mask
-    network_address = get_network_address(clArgs.ipaddress, clArgs.subnetmask)
-    if network_address == 'False':
-        print('Invalid IP and/or subnet mask')
-        exit(1)
-
     # ntwk is assigned class type ipaddress.IPv4Network
-    ntwk = ipaddress.IPv4Network(network_address)
+    ntwk = ipaddress.IPv4Network(get_network_address(clArgs.ipaddress, clArgs.subnetmask))
     
     # print to screen
     print(f'IPv4 Address: {clArgs.ipaddress}')
     print(f'Subnet Mask: {clArgs.subnetmask}')
+    print(f'Host Mask: {ntwk.hostmask}')
     print(f'Network Address: {ntwk.network_address}')
     print(f'Network Address with CIDR: {ntwk.network_address}/{ntwk.prefixlen}')
     print(f'First Assignable IP: {ntwk.network_address + 1}')
